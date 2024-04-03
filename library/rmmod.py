06377f6717d5e59c8235a7494b44804d3e74fc45
seahawk1986 <=>
2024-04-03 13:02:41 +0200

    Cleanup rmmod.py

--- library/rmmod.py
+++ library/rmmod.py
@@ -7,12 +7,11 @@
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 from collections import OrderedDict
-from typing import Generator
+from typing import Generator, List, Mapping
 import traceback
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils._text import to_native
-import subprocess
 
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
@@ -43,8 +42,11 @@ EXAMPLES = '''
 '''
 
 
-
-def find_dependencies(module, dependency_map, dependencies=[]):
+def find_dependencies(
+        module: str,
+        dependency_map: Mapping[str, List[str]],
+        dependencies: List[str]
+):
     dependencies.append(module)
     if module in dependency_map:
         for dependency in dependency_map[module]:
@@ -52,12 +54,13 @@ def find_dependencies(module, dependency_map, dependencies=[]):
     return dependencies
 
 
-def module_dependency_gen() -> Generator[str, list[str]]:
-    dependency_map = {}
+def module_dependency_gen() -> Generator[str, List[str]]:
     with open('/proc/modules') as f:
         for line in f:
             module_name, _, _, dependencies, *_ = line.split()
-            yield module_name, list(filter(lambda x: x not in ('', '-'), dependencies.split(',')))
+            yield module_name, list(
+                filter(lambda x: x not in ('', '-'), dependencies.split(','))
+            )
 
 
 def main():
@@ -79,19 +82,36 @@ def main():
     # Check if module is loaded
     try:
         dependency_map = dict(module_dependency_gen())
-        is_loaded = True if module_name in dependency_map else False
     except IOError as e:
-        module.fail_json(msg=to_native(e), exception=traceback.format_exc(), **result)
+        module.fail_json(
+            msg=to_native(e),
+            exception=traceback.format_exc(), **result
+        )
+    else:
+        is_loaded = True if name in dependency_map else False
+        
 
     # remove module if it is loaded
     if is_loaded:
         if not module.check_mode:
             # get a list of unique elements that keeps the original order
-            all_modules = list(OrderedDict.fromkeys(reversed(find_dependencies(name, dependency_map))))
+            all_modules = list(
+                OrderedDict.fromkeys(
+                    reversed(
+                        find_dependencies(name, dependency_map, [])
+                    )
+                )
+            )
             for kernel_module in all_modules:
-                rc, out, err = module.run_command([module.get_bin_path('rmmod', True), kernel_module])
+                rc, out, err = module.run_command(
+                    [module.get_bin_path('rmmod', True), kernel_module]
+                )
                 if rc != 0:
-                    module.fail_json(msg=err, rc=rc, stdout=out, stderr=err, **result)
+                    module.fail_json(
+                        msg=err,
+                        rc=rc,
+                        stdout=out, stderr=err, **result
+                    )
                 else:
                     result['unloaded_modules'].append(kernel_module)
             if result['unloaded_modules']:
